bd6e84071424d71f15c7822a5e300d33d9fb364f
SSkor <33691359+SSkor@users.noreply.github.com>
2018-03-25 11:21:53 +0300

    add new csv

--- ММФА/Minimum_variance frontier without risk_free assets.R
+++ ММФА/Minimum_variance frontier without risk_free assets.R
@@ -1,15 +1,34 @@
-vdata <- c(10, -10, 0, -10, 20, -20, 0, -20, 50)
-V <- solve(matrix(data=vdata, nrow=3, ncol=3)) #îáğàòíàÿ ìàòğèöà solve
+library(data.table)
+library(tseries)
+#Efficient Frontier with Short-Selling
+setwd("C:/Users/SSkorikova/Desktop")
+ROSN <- read.csv("ROSN.csv", header = TRUE, sep=",")[["Change.."]][1:21] #äîõîäíîñòè â %
+MGNT <- read.csv("MGNT.csv", header = TRUE, sep=",")[["Change.."]][1:21]
+MTSS <- read.csv("MTSS.csv", header = TRUE, sep=",")[["Change.."]][1:21]
+tickers <- data.frame(ROSN, MGNT, MTSS)
+vdata <- cov(tickers)
+V <- solve(vdata) #îáğàòíàÿ ìàòğèöà solve
 I <- rep(1, 3) #âåêòîğ-ñòîëáåö
-mu <- c(12, 13, 12) #âåêòîğ-ñòîëáåö
+mu <- apply(tickers, 2, mean)
 a <- as.numeric(t(I)%*%V%*%I) #àëüôà
 b <- as.numeric(t(I)%*%V%*%mu) #áåòà
 g <- as.numeric(t(mu)%*%V%*%mu) #ãàììà
 d <- as.numeric(a*g-b^2) #äåëüòà
-#MVF <- (a*m^2-2*b*m+g)/d
-curve(((a*x^2-2*b*x+g)/d)^(1/2),0,100)
-mumin <- b/a
+curve( ((a*x^2-2*b*x+g)/d)^(1/2), from=-1, to=1, xlab ="Expected Returns", ylab = "Volatility", 
+       main = "Efficient Frontier with Short-Selling") #MVF
+mumin <- min(mu)
+mumax <- max(mu)
 l <- (g-b*mumin)/d
 t <- (a*mumin-b)/d
 X <- V%*%(l*I+t*mu)
 qumin <- 1/sqrt(a)
+#without Short-Selling
+er_vals<- seq(from = mumin, to = mumax, length.out = 1000)
+sd_vals <- sapply(er_vals, function(er) {
+  op <- portfolio.optim(as.matrix(tickers), er)
+  return(op$ps)
+})
+plot_dt <- data.table(er = er_vals, sd = sd_vals)
+plot(plot_dt, main = "Efficient Frontier without Short-Selling", xlab ="Expected Returns", ylab = "Volatility")
+#CML
+
